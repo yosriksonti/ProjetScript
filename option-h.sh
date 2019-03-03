aae316d5f0964310a045b4013bd094bbcc6d9a4d
US1 <bessem.bousselmi@esprit.tn>
2019-03-03 20:10:21 +0100

    kernel update v2

--- option-h.sh
+++ option-h.sh
@@ -1,7 +1,6 @@
 
 function  Help () {
-  echo -e "\n \t \t \t ${RED}Welcome to WiScript Tool ${NC} \n "
-  echo -e "${YELLOW}-l: ${NC}Lister les différents points d'acces wifi."
+  echo -e "${YELLOW}-l: ${NC} Lister les différents points d'acces wifi."
   echo -e "${YELLOW}-lsort: ${NC} Lister les points d'acces wifi par ordre decroissant d'intensite de signal."
   echo -e "${YELLOW}-d: ${NC} Disactiver le service NetworkManager."
   echo -e "${YELLOW}-c: ${NC} Configurer manuellement l'accès a un point d'acces."
